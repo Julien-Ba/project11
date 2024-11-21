e7efb276e2414301da5c9a1e2e77c4b127386270
julien-ba <julienbarbier42@gmail.com>
2024-11-21 17:25:20 +0100

    pass const through props

--- kasa/src/pages/home/Home.jsx
+++ kasa/src/pages/home/Home.jsx
@@ -3,9 +3,11 @@ import Banner from '../../components/banner/Banner';
 import Gallery from '../../components/gallery/Gallery';
 
 export default function Home() {
+    const titleString = 'Chez vous, partout et ailleurs';
+
     return (
         <div className='home'>
-            <Banner title='Chez vous, partout et ailleurs' />
+            <Banner title={titleString} />
             <Gallery />
         </div>
     );
