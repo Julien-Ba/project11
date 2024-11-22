369407744e7beda2aa77bba61ef396dbe28ff542
julien-ba <julienbarbier42@gmail.com>
2024-11-22 09:53:56 +0100

    fixed key props

--- kasa/src/components/card/Card.jsx
+++ kasa/src/components/card/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Card({ accommodation }) {
     return (
         <Link to={`/accomodation/${accommodation.id}`}>
-            <article key={accommodation.id} className='card'>
+            <article className='card'>
                 <img src={accommodation.cover} alt={accommodation.title} className='card-img' />
                 <h2 className='card-title'>{accommodation.title}</h2>
             </article>
--- kasa/src/components/gallery/Gallery.jsx
+++ kasa/src/components/gallery/Gallery.jsx
@@ -1,12 +1,11 @@
 import './gallery.scss';
-import '../card/Card';
 import Card from '../card/Card';
 
 export default function Gallery({ accommodations }) {
     return (
         <section className='gallery'>
             {accommodations.map((accommodation) => (
-                <Card accommodation={accommodation} />
+                <Card key={accommodation.id} accommodation={accommodation} />
             ))}
         </section>
     );
