444e553dc424437788d6af576905e3e14cdf4601
julien-ba <julienbarbier42@gmail.com>
2024-11-22 09:52:21 +0100

    populated footer

--- kasa/src/components/footer/Footer.jsx
+++ kasa/src/components/footer/Footer.jsx
@@ -1,5 +1,11 @@
 import './footer.scss';
+import logo from '../../assets/logo.svg';
 
 export default function Footer() {
-    return <footer className='footer'></footer>;
+    return (
+        <footer className='footer'>
+            <img src={logo} alt='logo kasa' className='logo' />
+            <p className='copyright'>© 2020 Kasa. All rights reserved</p>
+        </footer>
+    );
 }
