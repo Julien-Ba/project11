0a4ab0f6169d41f17ec37ee0fde2a4369e52d396
julien-ba <julienbarbier42@gmail.com>
2024-11-22 11:09:10 +0100

    implemented accommodation page

--- kasa/src/pages/accommodation/Accommodation.jsx
+++ kasa/src/pages/accommodation/Accommodation.jsx
@@ -1,5 +1,25 @@
 import './accommodation.scss';
+import { useParams, Navigate } from 'react-router-dom';
+import { useAccommodations } from '../../hooks/useAccommodations';
+import Carousel from '../../components/carousel/Carousel';
 
-export default function Accomodation() {
-    return <div className='accomodation'></div>;
+export default function Accommodation() {
+    const { accommodations, loading, error } = useAccommodations();
+    const { id } = useParams();
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error loading data!</div>;
+
+    const accommodation = accommodations.find((acc) => acc.id === id);
+
+    if (!accommodation) {
+        return <Navigate to='/404' replace />;
+    }
+
+    return (
+        <div className='accommodation'>
+            <Carousel images={accommodation.pictures} />
+            <h1 className='accommodation-title'>{accommodation.title}</h1>
+        </div>
+    );
 }
