f73159578e5d9b360341314c54e77365e2222204
julien-ba <julienbarbier42@gmail.com>
2024-11-22 11:08:53 +0100

    implemented notfound page

--- kasa/src/pages/notFound/NotFound.jsx
+++ kasa/src/pages/notFound/NotFound.jsx
@@ -1,5 +1,14 @@
 import './notFound.scss';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
-    return <div className='notFound'></div>;
+    return (
+        <div className='notfound'>
+            <h1 className='error'>404</h1>
+            <h2 className='description'>Oups! La page que vous demandez n'existe pas.</h2>
+            <Link to='/' className='home-link'>
+                Retourner sur la page d’accueil
+            </Link>
+        </div>
+    );
 }
