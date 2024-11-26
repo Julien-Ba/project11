0e2db93ee383b04141110a1b402a66b7ff3ca6a2
julien-ba <julienbarbier42@gmail.com>
2024-11-26 16:23:39 +0100

    use hashRouter for better compatibility with github pages

--- kasa/src/router/router.jsx
+++ kasa/src/router/router.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import About from '../pages/about/About';
 import Accommodation from '../pages/accommodation/Accommodation';
 import Home from '../pages/home/Home';
 import NotFound from '../pages/notFound/NotFound';
 import Layout from '../components/layout/Layout';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         element: <Layout />,
         children: [
